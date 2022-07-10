29eca68866e405bbede455943a6cb1e7721ab573
Burt <2902978956@qq.com>
2022-07-10 10:11:00 +0800

    index 路由页面添加 css 样式

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,10 @@
+import stylesUrl from '~/styles/index.css';
+import type {LinksFunction} from '@remix-run/node';
+
+export const links: LinksFunction = () => [
+  {rel: 'stylesheet', href: stylesUrl},
+];
+
 export default function IndexRoute() {
-  return <div>Hello Index Route</div>
+  return <div>Hello Index Route</div>;
 }
